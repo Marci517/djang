08fd9c64e3afa53cdb644408ff6e2aa07b504087
Bencze Márton <benczemarton38@gmail.com>
2024-07-23 14:03:28 +0300

    delete buttons and functionality

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,9 @@ from . import views
 #URL config
 
 urlpatterns = [
+    path('', views.get_all_books),
     path('getBookByName/<str:name>', views.get_book_by_name),
     path('getBooksByAuthor/<str:name>', views.get_books_by_author),
     path('getBooksByCategory/<str:category>', views.get_books_by_category),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import models
 
 def get_book_by_name(req, name):
@@ -13,9 +13,6 @@ def get_books_by_category(req, category):
         books =  models.Book.objects.filter(category = category)
     except models.Book.DoesNotExist:
         books = None
-
-    if len(books) == 1:
-        return render(req, 'book.html', {'book': books.first()})
     
     return render(req, 'books.html', {'books': books})
 
@@ -24,8 +21,21 @@ def get_books_by_author(req, name):
         books =  models.Book.objects.filter(author__name = name)
     except models.Book.DoesNotExist:
         books = None
-
-    if len(books) == 1:
-        return render(req, 'book.html', {'book': books.first()})
-    
     return render(req, 'books.html', {'books': books})
+
+
+def get_all_books(req):
+    books = models.Book.objects.all()
+    categories = {}
+    for book in books:
+        if book.category not in categories:
+            categories[book.category] = []
+        categories[book.category].append(book)
+    
+    return render(req, 'home.html', {'categories': categories})
+
+
+def delete_book(req, book_id):
+    book = models.Book.objects.get(id=book_id)
+    book.delete()
+    return redirect('books')
