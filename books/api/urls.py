6646d451974e2d0c99e1b975ce59335b31da97a3
Bencze Márton <benczemarton38@gmail.com>
2024-07-29 09:57:16 +0300

    get apis done

--- books/api/urls.py
+++ books/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jsonbooks/', views.get_books)
+    path('all/', views.get_books),
+    path('name/<str:name>/', views.get_book_by_name),
+    path('category/<str:cat>/', views.get_books_by_category),
+    path('author/<str:name>/', views.get_books_by_author)
 ]
--- books/api/views.py
+++ books/api/views.py
@@ -6,5 +6,32 @@ from .serializers import BookSerializer
 @api_view(['GET'])
 def get_books(req):
     books = Book.objects.all()
-    serialized_books = BookSerializer(books, many = True)
-    return Response(serialized_books.data)
+    if books.exists():
+        serialized_books = BookSerializer(books, many = True)
+        return Response(serialized_books.data)
+    return Response('There are no books')
+
+@api_view(['GET'])
+def get_book_by_name(req, name):
+    try:
+        book = Book.objects.get(title = name)
+        serialized_book = BookSerializer(book)
+        return Response(serialized_book.data)
+    except Book.DoesNotExist:
+        return Response('there is no such a book')
+    
+@api_view(['GET'])
+def get_books_by_category(req, cat):
+    books = Book.objects.filter(category = cat)
+    if books.exists():
+        serialized_books = BookSerializer(books, many = True)
+        return Response(serialized_books.data)
+    return Response('there is no such a book')
+    
+@api_view(['GET'])
+def get_books_by_author(req, name):
+    books = Book.objects.filter(author__name = name)
+    if books.exists():
+        serialized_books = BookSerializer(books, many = True)
+        return Response(serialized_books.data)
+    return Response('there is no such a book')
